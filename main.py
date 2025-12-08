31748ad9a1d0e7c2cec048f7b57d33792d3b8c4a
PunishedPlebe <mylescharlesworth@icloud.com>
2025-12-08 07:12:37 -0500

    Imported sys package to use sys.argv to capture user-input when running the program from CLI to allow dynamic file useage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import word_count
 from stats import character_count
 from stats import dict_sort
+import sys
 
 def get_book_contents(filepath):
     book_text = None
@@ -9,19 +10,23 @@ def get_book_contents(filepath):
     return book_text
 
 def main():
-    file_path = "books/frankenstein.txt"
-    dict_lst = dict_sort(character_count(get_book_contents(file_path)))
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {file_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {word_count(get_book_contents(file_path))} total words")
-    print("--------- Character Count -------")
-    for dict in dict_lst:
-        if dict["char"].isalpha() == True:
-            print(f"{dict["char"]}: {dict["num"]}")
-        else:
-            continue
-    print("============= END ===============")
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        print('Usage: python3 main.py <path_to_book>')
+        sys.exit(1)
+    else:
+        file_path = sys.argv[1]
+        dict_lst = dict_sort(character_count(get_book_contents(file_path)))
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {file_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {word_count(get_book_contents(file_path))} total words")
+        print("--------- Character Count -------")
+        for dict in dict_lst:
+            if dict["char"].isalpha() == True:
+                print(f"{dict["char"]}: {dict["num"]}")
+            else:
+                continue
+        print("============= END ===============")
 
 
 
